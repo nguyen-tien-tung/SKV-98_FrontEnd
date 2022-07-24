b349c5ce44592f7b22daa1da6f6f7c0db54b2a93
Tung Nguyen <ngtientung98@gmail.com>
2022-07-24 22:49:23 +0700

    change intro video url

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -125,7 +125,7 @@ const Home = () => {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/QDOfpy6LyRQ"
+            src="https://www.youtube.com/watch?v=XxbzU6l94E8&t=1s"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
